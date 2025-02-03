38282ffe82b2d24fa54c32f6e346e9a1cf8eb825
Arturo Orient Romero <arturorient1305@gmail.com>
2025-02-03 13:53:03 +0100

    Update greeting message in index function for improved clarity

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ def index():
 
     print(username, email, password)
 
-    return '<h1>Hola mundo hermoso sabroson. <br>{} <br>{} <br>{}</h1>'.format(username, email, password)
+    return '<h1>Mi primera y mejor APP Flask deployeada. <br>{} <br>{} <br>{}</h1>'.format(username, email, password)
 
 def status_404(error):
     return '<h2>PAGINA NO ENCONTRADA, ABANDONAR  LA PAGINA INMEDIATAMENTE O UN GATO SAUDI ACABARA CON TU FAMILIA</h2> <img src="https://i.pinimg.com/736x/60/72/bd/6072bd387f10105f4cca73158aadd49b.jpg">'
